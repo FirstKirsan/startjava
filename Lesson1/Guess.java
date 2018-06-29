02a5e4beff0a9a62861a6bc692ac03f3062d3e98
Kirill <kirbelom@gmail.com>
2018-06-29 09:25:34 +0900

    HW1 Guess

--- Lesson1/Guess.java
+++ Lesson1/Guess.java
@@ -1,15 +1,21 @@
 public class Guess {
 	public static void main(String[] args) {
-		int compNumber = 64;
-		int myNumber = 14;
+		int compNumber = 100;
+		int myNumber = 63;
+		int max = 101;
+		int min = 0;
 
 		while (compNumber != myNumber) {
 			if (compNumber > myNumber) {
 				System.out.println("Введенное вами число меньше того, что загадал компьютер");
-
+				min = myNumber;
+				myNumber += (max - myNumber)/2;
+				System.out.println(myNumber);
 			} else {
 				System.out.println("Введенное вами число больше того, что загадал компьютер");
-				myNumber /= 2;
+				max = myNumber;
+				myNumber = min + (myNumber - min)/2;
+				System.out.println(myNumber);
 			}
 
 		}
