1071fb424e2bf3f3550df1222315afd2b77de665
Kirill <kirbelom@gmail.com>
2018-07-14 20:32:55 +0900

    L2 14_07

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class GuessNumberTest {
 
     public static void main(String[] args) {
-        boolean game = true;
-        Player player1 = new Player("Vasya");
-        Player player2 = new Player("Masha");
-        GuessNumber guessNumber = new GuessNumber(player1, player2);
+        boolean game = true;       
         Scanner scan = new Scanner(System.in);
         String continueGame;
+        System.out.print("Игрок 1, введите имя: ");
+        Player player1 = new Player(scan.nextLine());
+        System.out.print("Игрок 2, введите имя: ");
+        Player player2 = new Player(scan.nextLine());
+        GuessNumber guessNumber = new GuessNumber(player1, player2);
 
         while (game) {
             guessNumber.start();
