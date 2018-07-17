57beca33ccdfbe29331edf83c5d3d5fac505ffcf
Kirill <kirbelom@gmail.com>
2018-07-17 20:59:07 +0900

    L2 Calc 17_07-2

--- Lesson2/Calc.java
+++ Lesson2/Calc.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class Calc {
-	private Scanner scan = new Scanner(System.in);
+	String mathOperation;
+    int numberOne;
+    int numberTwo;
 
-	public void start(int numberOne, String mathOperation, int numberTwo) {
+	public void start() {
 		switch (mathOperation) {
 			case "*": 
 				System.out.println(numberOne * numberTwo);
@@ -21,15 +23,16 @@ public class Calc {
 				System.out.println(numberOne % numberTwo);
 				break;
 			case "^":
-				System.out.println(doDegree(numberOne, numberTwo));
+				System.out.println(pow(numberOne, numberTwo));
 				break;
-			default: System.out.println("Unknown mathOperation");
+			default: 
+				System.out.println("Неизвестная операция");
 		}
 	}
-	public int doDegree (int numberOne, int numberTwo) {
-		int result =1;
-		for (int i = 0; i < numberTwo; i++) {
-			result *= numberOne;
+	public int pow (int base, int power) {
+		int result = 1;
+		for (int i = 0; i < power; i++) {
+			result *= base;
 		}
 		return result;
 	}
--- Lesson2/CalcTest.java
+++ Lesson2/CalcTest.java
@@ -2,22 +2,18 @@ import java.util.Scanner;
 
 public class CalcTest {
 	public static void main (String[] args) {
-		boolean calc = true;
 		String answer = "да";
 		Scanner scan = new Scanner(System.in);
-        String mathOperation;
-        int numberOne;
-        int numberTwo;
-        Calc calcProgram = new Calc();
+        Calc calc = new Calc();
 
         while (answer.equals("да")) {
             System.out.print("Введите первое число: ");
-            numberOne = scan.nextInt();
+            calc.numberOne = scan.nextInt();
             System.out.print("Введите знак математической операции: ");
-            mathOperation = scan.next();
+            calc.mathOperation = scan.next();
             System.out.print("Введите второе число: ");
-            numberTwo = scan.nextInt();
-            calcProgram.start(numberOne, mathOperation, numberTwo);
+            calc.numberTwo = scan.nextInt();
+            calc.start();
             do {
                 System.out.print("Хотите продолжить? [да/нет]: ");
                 answer = scan.next();
